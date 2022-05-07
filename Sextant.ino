0b07a8bad6bd861b9caa69a3cc44d08470b60450
h-blank <hbl1984@gmx.de>
2022-05-07 11:01:48 +0200

    Ausgabe von x-Achse

--- Sextant.ino
+++ Sextant.ino
@@ -1,8 +1,11 @@
 #include <Arduino_LSM6DS3.h>
-#include <LiquidCrystal.h>
 
+#include <LiquidCrystal.h>
 const int rs = 8, en = 9, d4 = 4, d5 = 5, d6 = 6, d7 = 7;
 LiquidCrystal lcd(rs,en,d4,d5,d6,d7);
+
+#include "Adafruit_VEML6070.h"
+Adafruit_VEML6070 uv = Adafruit_VEML6070();
 
 void setup() {
   Serial.begin(9600);
@@ -22,4 +25,5 @@ void loop() {
   float x,y,z;
 
   IMU.readGyroscope(x,y,z);
+  Serial.println(x);
 }
